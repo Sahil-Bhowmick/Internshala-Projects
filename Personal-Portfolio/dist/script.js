9b631c04946f53dd0a48815e49c5f7c89921433b
Sahil Bhowmick <sahilbhowmick1@gmail.com>
2024-08-24 22:59:11 +0530

    final commit

--- Personal-Portfolio/dist/script.js
+++ Personal-Portfolio/dist/script.js
@@ -1,9 +1,19 @@
-const sideMenu = document.querySelector("#sideMenu");
-
-function onenMenu() {
-  sideMenu.style.transform = "translateX(-16rem)";
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
 }
 
-function closeMenu() {
-  sideMenu.style.transform = "translateX(16rem)";
+function toggleTheme() {
+  document.documentElement.classList.toggle("dark");
+
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.theme = "dark";
+  } else {
+    localStorage.theme = "light";
+  }
 }
--- Personal-Portfolio/tailwind.config.js
+++ Personal-Portfolio/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+      colors: {
+        lightHover: "#fcf4ff",
+        darkHover: "#1f2937",
+        darkTheme: "#1b1b1b",
+      },
+      boxShadow: {
+        black: "4px 4px 0 #000",
+        white: "4px 4px 0 #fff",
+      },
+    },
   },
+  darkMode: "class",
   plugins: [],
 };
